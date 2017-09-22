4f6272e26c44bda453f28a6702a7fecd7208016b
Ryan Birmingham <birm@rbirm.us>
2017-09-22 11:34:15 -0400

    v1 of hubspoke

--- HubSpoke.js
+++ HubSpoke.js
@@ -6,64 +6,68 @@
  */
 class Spoke {
     constructor(hub_id, id, callback) {
-      this.hub_id = hub_id
-      this.id = id;
-      this.callback = callback;
-      let self = this;
-      window.addEventListener('storage', function(e){
-        // TODO if it's the hub, listen!
-      });
+        this.hub_id = hub_id
+        this.id = id;
+        this.callback = callback;
+        var self = this;
+        // listen to the hub
+        window.addEventListener('storage', function(e) {
+            if (e.key == self.hub_id) {
+                callback(e.newValue);
+            }
+        });
     }
-    set(value){
-      /* set this spoke's state */
-      window.localStorage.setItem(this.key, value);
+    set(value) {
+        /* set this spoke's state */
+        window.localStorage.setItem(this.key, value);
     }
-    get(){
-      /* get parent state */
-      return window.localStorage.getItem(this.hub_id);
+    get() {
+        /* get parent state */
+        return window.localStorage.getItem(this.hub_id);
     }
 
 }
 
 /* Hub
  * @constructor
-  * @param callback - what to do with new information
+ * @param callback - what to do with new information
  */
 class Hub {
-    // TODO more sensible method names
     constructor(callback) {
-      this.spokes = [];
-      this.callback = callback;
-      // INSECURE HASH FUNCTION to avoid collision
-      var dt = new Date().toString(),
-          hash = 0;
-      for (var i = 0; i < dt.length; i++) {
-          var char = dt.charCodeAt(i);
-          hash = ((hash << 5) - hash) + char;
-          hash = hash & hash; // Convert to 32bit integer
-          hash = Math.abs(hash);
-      }
-      this.hash = hash;
-      window.addEventListener('storage', function(e){
-        // TODO if it's a spoke, listen!
-      });
+        this.spokes = [];
+        this.callback = callback;
+        // INSECURE HASH FUNCTION to avoid collision
+        var dt = new Date().toString(),
+            hash = 0;
+        for (var i = 0; i < dt.length; i++) {
+            var char = dt.charCodeAt(i);
+            hash = ((hash << 5) - hash) + char;
+            hash = hash & hash; // Convert to 32bit integer
+            hash = Math.abs(hash);
+        }
+        this.hash = hash;
+        var self = this;
+        // listen to any of the spokes
+        window.addEventListener('storage', function(e) {
+            if (e.key in self.spokes) {
+                callback(e.newValue);
+            }
+        });
 
     }
 
-    register_spoke(id){
-      /* Add a Spoke
-       *@param id - the id to use for the spoke
-       */
-       let newspoke = this.hash + "-" + id;
-       this.spokes.push(newspoke);
-       // TODO add listen to this new spoke
+
+    register_spoke(id) {
+        /* Add a Spoke
+         *@param id - the id to use for the spoke
+         */
+        let newspoke = this.hash + "-" + id;
+        this.spokes.push(newspoke);
+        // TODO add listen to this new spoke
     }
-    set(value){
-      /* set this hub's state */
-      window.localStorage.setItem(this.hash, value);
+    set(value) {
+        /* set this hub's state */
+        window.localStorage.setItem(this.hash, value);
     }
-    // TODO this needs to listen to its spokes
-    listen(callback){
-      // TODO set up listen callback event
-    }
+
 }
