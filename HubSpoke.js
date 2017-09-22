6455f9ff47ce14f1d8872e07c2b6b26442970fa6
Ryan Birmingham <birm@rbirm.us>
2017-09-22 16:26:09 -0400

    v2 hubspoke done

--- HubSpoke.js
+++ HubSpoke.js
@@ -1,47 +1,42 @@
-/* Spoke
+/* Spoke - communicate state information with hub using (this).state = {Value}
  * @constructor
  * @param hub_id - the hub's hash, and this item's base hash
  * @param id - identifier for the spoke given the parent
- * @param callback - what to do with new information
  */
 class Spoke {
-    constructor(hub_id, id, callback) {
+    constructor(hub_id, id) {
         this.hub_id = hub_id
         this.id = hub_id + "-" + id;
-        this.callback = callback;
         this._state = "";
+        window.addEventListener('storage', (e) => this.listen(e));
     }
 
-    cats(event){
-      // TODO why is {this} a window
-      console.log(this);
-      if (event.key == this.hub_id) {
-          this._state = event.newValue;
+    listen(event){
+      if (event.key == this.hub_id){
+        this._state = event.newValue;
       }
     }
 
     set state (value) {
-        /* set this spoke's state */
+        /* set this hub's state and update hub */
         this._state = value;
         window.localStorage.setItem(this.id, value);
     }
     get state() {
         /* get parent state */
         // the last hub state is the state we need alyways
-        return window.localStorage.getItem(this.hub_id);
+        return this._state;
     }
 
 }
 
-/* Hub
+/* Hub - communicate state information with spokes using (this).state = {Value}
  * @constructor
  * @param callback - what to do with new information
  */
 class Hub {
-    constructor(callback) {
-        this.spokes = [];
+    constructor() {
         this._state = "";
-        this.callback = callback;
         // INSECURE HASH FUNCTION to avoid collision
         var dt = new Date().toString(),
             hash = 0;
@@ -52,29 +47,22 @@ class Hub {
             hash = Math.abs(hash);
         }
         this.id = hash;
+        window.addEventListener('storage', (e) => this.listen(e));
     }
 
-    cats(event){
-      // TODO why is {this} a window
-      console.log(this);
-      if (this.spokes.includes(event.key)) {
-          this._state = event.newValue;
+    // TODO confirm it won't self trigger. I think storage doesn't apply in window
+    listen(event){
+      if (event.key.split("-")[0] == this.id){
+        this._state = event.newValue;
       }
     }
 
-    register_spoke(id) {
-        /* Add a Spoke
-         *@param id - the id to use for the spoke
-         */
-        this.spokes.push(id);
-    }
     set state(value) {
-        /* set this hub's state */
+        /* set this hub's state and update spokes*/
         this._state = value;
         window.localStorage.setItem(this.id, value);
     }
     get state(){
-      // TODO update this._state with new registerable events
       // the state is the last updated of all states
       return this._state;
     }
